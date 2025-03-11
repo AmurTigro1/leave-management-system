6df66e791bbcdc608e02614e6c05defc80a4f28e
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-03-11 15:19:13 +0800

    fixed profile and sidebar

--- resources/views/employee/partials/sidebar.blade.php
+++ resources/views/employee/partials/sidebar.blade.php
@@ -71,7 +71,7 @@
         </div>
     </div>    
 
-    <a href="{{ route('employee.leave_request') }}" class="hover:bg-gray-200 flex items-center p-2 space-x-2 rounded-md {{ request()->routeIs('holiday.calendar') ? 'bg-white shadow-lg' : 'text-gray-500' }}">
+    <a href="{{ route('employee.leave_request') }}" class="hover:bg-gray-200 flex items-center p-2 space-x-2 rounded-md {{ request()->routeIs('employee.leave_request') ? 'bg-white shadow-lg' : 'text-gray-500' }}">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
         </svg>          
