53339e2915bc4732f3fdca26fdbbb729654cdc2e
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-03-18 15:37:04 +0800

    users account

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
             'first_name' => ['string', 'max:255'],
             'middle_name' => ['string', 'max:255'],
             'last_name' => ['string', 'max:255'],
-            'birthday' => ['required', 'date'],
+            'birthday' => ['nullable', 'date'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
