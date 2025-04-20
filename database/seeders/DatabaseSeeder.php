e27fa571f5fb78ea01fbc5db34638b3858dce29e
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2025-04-21 01:54:20 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(HRSupervisorSeeder::class);
+        $this->call(HrSupervisorSeeder::class);
         // User::factory(10)->create();
 
         User::factory()->create([
