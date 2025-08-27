eec07e5e71f7d6fa87c1a8f8ad2b99ccdc08a20b
Stern Salera <sternsalera878@gmail.com>
2025-08-27 16:31:48 +0800

    fixed decimal for sick and leave balance

---
+++ database/migrations/2025_08_27_082825_alter_leave_balance_precision_in_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('vacation_leave_balance', 8, 3)->change();
+            $table->decimal('sick_leave_balance', 8, 3)->change();
+            $table->decimal('mandatory_leave_balance', 8, 3)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('vacation_leave_balance', 8, 2)->change();
+            $table->decimal('sick_leave_balance', 8, 2)->change();
+            $table->decimal('mandatory_leave_balance', 8, 2)->change();
+        });
+    }
+};
